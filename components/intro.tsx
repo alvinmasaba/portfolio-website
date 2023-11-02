e38a26709c3e351c647d92d3baed391dc90a5761
Alvin <alvinmasaba@gmail.com>
2023-11-02 01:49:05 -0400

    Updates CV path.

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,18 +104,23 @@ export default function Intro() {
           className='opacity-70 group-hover:translate-x-1 transition'/>
         </Link>
 
-        <a className='group bg-white px-7 py-3 
-        flex items-center gap-2 rounded-full
-        outline-none focus:scale-110 
-        hover:scale-110 active:scale-105 
-        transiition cursor-pointer borderBlack
-        dark:bg-white/10' 
-        href='../public/cv.pdf' download>
-          Download CV <HiDownload 
-          className='opacity-60 
-          group-hover:translate-y-1 transition'/>
+        <a 
+          className='group bg-white px-7 py-3 
+          flex items-center gap-2 rounded-full
+          outline-none focus:scale-110 
+          hover:scale-110 active:scale-105 
+          transiition cursor-pointer borderBlack
+          dark:bg-white/10' 
+          href='/CV.pdf' 
+          download
+        >
+          Download CV 
+          <HiDownload 
+            className='opacity-60 
+            group-hover:translate-y-1 
+            transition'
+          />
         </a>
-
         <a className='bg-white p-4 text-gray-700
         flex items-center gap-2 rounded-full
         focus:scale-[1.15] hover:scale-[1.15]
